22a81b7783150df8d1665045c906240b6e833932
Soma-Ghosh <somabellywoods@gmail.com>
2021-06-30 22:29:49 +0530

    contactform

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -1,8 +1,7 @@
-/**
+/*
 * PHP Email Form Validation - v3.1
 * URL: https://bootstrapmade.com/php-email-form/
 * Author: BootstrapMade.com
-
 (function () {
   "use strict";
 
@@ -83,3 +82,4 @@
   }
 
 })();*/
+
